17270a6dbd38d6b2be028f01a9df993f99059cd1
Jairo Gonzalez <jairo99gc@icloud.com>
2021-06-18 10:12:24 -0500

    Inicio del proyecto

---
+++ mvc_ingenieria_web/lib/Models/students.dart
@@ -0,0 +1,15 @@
+class Student {
+  String name;
+  String lastname;
+  String id;
+  String number;
+  int age;
+
+  Student(String name, String lastname, String id, String number, int age) {
+    this.name = name;
+    this.lastname = lastname;
+    this.id = id;
+    this.number = number;
+    this.age = age;
+  }
+}
